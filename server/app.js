af62b9c014fa85d8bbd0d6ececba03255a302041
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-02-28 16:17:22 -0500

    added https server path
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- server/app.js
+++ server/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const path = require("path");
+
+const { HTTP_PORT, HTTPS_PORT } = require('./config.json');
+console.log(HTTPS_PORT);
+
+
+const privateKey = fs.readFileSync(path.resolve(__dirname, "../sslcert/server.key"), 'utf8');
+const certificate = fs.readFileSync(path.resolve(__dirname, "../sslcert/server.cert"), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+
+
 const app = express();
+
+
 const port = 3000;
 
 const routes = require('./routes/index.route');
@@ -8,6 +25,11 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(HTTP_PORT);
+httpsServer.listen(HTTPS_PORT);
 
 module.exports = { app };
--- server/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`starter listening on http://localhost:${port}`)
-});
