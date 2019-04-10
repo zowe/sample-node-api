7c94b8f009cf0596e431be8be5af5f02e6a9eda0
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-04-10 13:34:15 -0400

    some unix system use outputs port in column 2
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- scripts/restart-sample-node-api.sh
+++ scripts/restart-sample-node-api.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo 'restarting sample node api...'
+# ps -elf | awk '/node/ && /app.js/ && !/sh -c/' | awk '{print $2}' | xargs kill -9 $1
 ps -elf | awk '/node/ && /app.js/ && !/sh -c/' | awk '{print $3}' | xargs kill -9 $1
 ./start-sample-node-api.sh
 sleep 30
