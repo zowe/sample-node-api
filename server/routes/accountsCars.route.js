6aa526a638a609932d2cb851d4df09973a320d19
Nakul Manchanda <nakul.manchanda@ibm.com>
2020-02-09 01:03:32 -0500

    removed extra file, un-comment /cars api
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- server/routes/accountsCars.route.js
+++ server/routes/accountsCars.route.js
@@ -13,7 +13,7 @@ const router = express.Router({ mergeParams: true });
 
 const accountsCarsController = require('../controllers/accountsCars.controller');
 
-/*
+
 // Add missing feature
 // un-comment this to implement two new routes
 // accounts/:id/cars & accounts/:id/cars/:id2
@@ -23,6 +23,6 @@ router.route('/cars')
 
 router.route('/cars/:_id')
     .get(accountsCarsController.get);
-*/
+
 
 module.exports = router;
